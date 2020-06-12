e6c77ff787f69f958a4daaa378a981337b577786
gavimar <gavimar182@gmail.com>
2020-06-12 12:07:23 +0200

    deploy

---
+++ docs/precache-manifest.42d29be9b5eed67c63b15020e243f935.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7014aa485772b52be2832063af919e30",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e5b738b7f6a640fb2640",
+    "url": "./static/css/main.ffa29014.chunk.css"
+  },
+  {
+    "revision": "c8ce21eb042927bb8a22",
+    "url": "./static/js/2.bec1e834.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.bec1e834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b738b7f6a640fb2640",
+    "url": "./static/js/main.df6f11c5.chunk.js"
+  },
+  {
+    "revision": "dc883005ecfe893a9ece",
+    "url": "./static/js/runtime-main.37bffac9.js"
+  },
+  {
+    "revision": "ae24d4c47581a333e685ea65c006eb0a",
+    "url": "./static/media/loading2.ae24d4c4.gif"
+  }
+]);
